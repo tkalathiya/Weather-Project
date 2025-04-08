419fc1c9512267217ff28e7d3e413e4e5d0984a3
Tanish Naresh Kalathiya <tkalathiya@myseneca.ca>
2025-04-08 19:34:04 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from typing import Optional
+# main.py
+from fastapi import FastAPI, Query
+import joblib
+import numpy as np
 
-from fastapi import FastAPI
+# Load model
+model = joblib.load("weather_model.pkl")
 
+# Create FastAPI app
 app = FastAPI()
 
+@app.get("/predict/")
+def predict(
+    precipitation: float = Query(..., description="precipitation"),
+    rain: float = Query(..., description="rain"),
+    humidity: float = Query(..., description="humidity")
+):
+    # Convert input to numpy array
+    input_array = np.array([[precipitation, rain, humidity]])
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    # Get prediction
+    prediction = model.predict(input_array)[0]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return {"prediction": int(prediction)}
+
+
+
+#Example Usuage:http://127.0.0.1:8000/predict/?precipitation=5.0&rain=3.0&humidity=1.0
